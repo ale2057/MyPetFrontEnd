c918ba78178e9c1e2e51a12406a414a7b154c5c6
Gabriel Torrico <ale.torrico@outlook.com>
2022-12-05 23:15:58 -0400

    adding footer and header

---
+++ src/app/Interfaces/Photo.ts
@@ -0,0 +1,5 @@
+export interface Photo{
+  id?:number;
+  petPhoto:string;
+  petId:number;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponentComponent } from './Components/home-component/home-component.component';
+
+//Angular Material
+import { SharedModule } from './Shared/shared.module';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HeaderComponent } from './Components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
